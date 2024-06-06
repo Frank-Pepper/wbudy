55561238ceaae8f9a21ddb030551eff6a7b2a282
Frank <reppepf@gmail.com>
2024-06-06 22:32:51 +0200

    code formatting

--- Alarm Clock/demo/src/main.c
+++ Alarm Clock/demo/src/main.c
@@ -29,6 +29,26 @@
 #define NOTE_PIN_LOW()  GPIO_ClearValue(0, 1<<26);
 
 
+static void playNote(uint32_t note, uint32_t durationMs);
+static uint32_t getNote(uint8_t ch);
+static uint32_t getDuration(uint8_t ch);
+static uint32_t getPause(uint8_t ch);
+static void playSong(uint8_t *song);
+static void init_ssp(void);
+static void init_i2c(void);
+static void init_adc(void);
+static void setUpRTC(void);
+static void handle_joystick(void);
+
+static void saveTimeToEEPROM(const RTC_TIME_Type *rtc);
+static void readTimeFromEEPROM(RTC_TIME_Type *rtc);
+static void saveAlarmToEEPROM(uint8_t hours,  uint8_t minutes, uint8_t seconds);
+static void setTime();
+
+void SysTick_Handler(void);
+uint32_t getTicks(void);
+
+
 static uint32_t notes[] = {
         2272, // A - 440 Hz
         2024, // B - 494 Hz
@@ -46,183 +66,9 @@ static uint32_t notes[] = {
         1275, // g - 784 Hz
 };
 
-static void playNote(uint32_t note, uint32_t durationMs) {
-
-    uint32_t t = 0;
-
-    if (note > 0) {
-
-        while (t < (durationMs*1000)) {
-            NOTE_PIN_HIGH();
-            Timer0_us_Wait(note / 2);
-            //delay32Us(0, note / 2);
-
-            NOTE_PIN_LOW();
-            Timer0_us_Wait(note / 2);
-            //delay32Us(0, note / 2);
-
-            t += note;
-        }
-
-    }
-    else {
-    	Timer0_Wait(durationMs);
-        //delay32Ms(0, durationMs);
-    }
-}
-
-static uint32_t getNote(uint8_t ch)
-{
-    if (ch >= 'A' && ch <= 'G')
-        return notes[ch - 'A'];
-
-    if (ch >= 'a' && ch <= 'g')
-        return notes[ch - 'a' + 7];
-
-    return 0;
-}
-
-static uint32_t getDuration(uint8_t ch)
-{
-    if (ch < '0' || ch > '9')
-        return 400;
-
-    /* number of ms */
-
-    return (ch - '0') * 200;
-}
-
-static uint32_t getPause(uint8_t ch)
-{
-    switch (ch) {
-    case '+':
-        return 0;
-    case ',':
-        return 5;
-    case '.':
-        return 20;
-    case '_':
-        return 30;
-    default:
-        return 5;
-    }
-}
-
-static void playSong(uint8_t *song) {
-    uint32_t note = 0;
-    uint32_t dur  = 0;
-    uint32_t pause = 0;
-
-    /*
-     * A song is a collection of tones where each tone is
-     * a note, duration and pause, e.g.
-     *
-     * "E2,F4,"
-     */
-
-    while(*song != '\0') {
-        note = getNote(*song++);
-        if (*song == '\0')
-            break;
-        dur  = getDuration(*song++);
-        if (*song == '\0')
-            break;
-        pause = getPause(*song++);
-
-        playNote(note, dur);
-        //delay32Ms(0, pause);
-        Timer0_Wait(pause);
-
-    }
-}
-
 static uint8_t * song = (uint8_t*)"C2.C2,D4,C4,F4,E8,";
         //(uint8_t*)"C2.C2,D4,C4,F4,E8,C2.C2,D4,C4,G4,F8,C2.C2,c4,A4,F4,E4,D4,A2.A2,H4,F4,G4,F8,";
         //"D4,B4,B4,A4,A4,G4,E4,D4.D2,E4,E4,A4,F4,D8.D4,d4,d4,c4,c4,B4,G4,E4.E2,F4,F4,A4,A4,G8,";
-
-
-
-static void init_ssp(void)
-{
-	SSP_CFG_Type SSP_ConfigStruct;
-	PINSEL_CFG_Type PinCfg;
-
-	/*
-	 * Initialize SPI pin connect
-	 * P0.7 - SCK;
-	 * P0.8 - MISO
-	 * P0.9 - MOSI
-	 * P2.2 - SSEL - used as GPIO
-	 */
-	PinCfg.Funcnum = 2;
-	PinCfg.OpenDrain = 0;
-	PinCfg.Pinmode = 0;
-	PinCfg.Portnum = 0;
-	PinCfg.Pinnum = 7;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 8;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 9;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Funcnum = 0;
-	PinCfg.Portnum = 2;
-	PinCfg.Pinnum = 2;
-	PINSEL_ConfigPin(&PinCfg);
-
-	SSP_ConfigStructInit(&SSP_ConfigStruct);
-
-	// Initialize SSP peripheral with parameter given in structure above
-	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
-
-	// Enable SSP peripheral
-	SSP_Cmd(LPC_SSP1, ENABLE);
-
-}
-
-static void init_i2c(void)
-{
-	PINSEL_CFG_Type PinCfg;
-
-	/* Initialize I2C2 pin connect */
-	PinCfg.Funcnum = 2;
-	PinCfg.Pinnum = 10;
-	PinCfg.Portnum = 0;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 11;
-	PINSEL_ConfigPin(&PinCfg);
-
-	// Initialize I2C peripheral
-	I2C_Init(LPC_I2C2, 100000);
-
-	/* Enable I2C1 operation */
-	I2C_Cmd(LPC_I2C2, ENABLE);
-}
-
-static void init_adc(void)
-{
-	PINSEL_CFG_Type PinCfg;
-
-	/*
-	 * Init ADC pin connect
-	 * AD0.0 on P0.23
-	 */
-	PinCfg.Funcnum = 1;
-	PinCfg.OpenDrain = 0;
-	PinCfg.Pinmode = 0;
-	PinCfg.Pinnum = 23;
-	PinCfg.Portnum = 0;
-	PINSEL_ConfigPin(&PinCfg);
-
-	/* Configuration for ADC :
-	 * 	Frequency at 0.2Mhz
-	 *  ADC channel 0, no Interrupt
-	 */
-	ADC_Init(LPC_ADC, 200000);
-	ADC_IntConfig(LPC_ADC,ADC_CHANNEL_0,DISABLE);
-	ADC_ChannelCmd(LPC_ADC,ADC_CHANNEL_0,ENABLE);
-
-}
-
 
 
 /* Zmienne do przechowywania aktualnego czasu */
@@ -248,169 +94,12 @@ const uint8_t margin = 5;
 
 volatile uint32_t msTicks; /* counts 1ms timeTicks */
 
-void SysTick_Handler(void) {
-    msTicks++;
-}
-
-uint32_t getTicks(void) {
-    return msTicks;
-}
-
-
-
-/* Inicjalizacja RTC */
-static void setUpRTC(void) {
-
-    RTC_Init(LPC_RTC);
-    RTC_ResetClockTickCounter(LPC_RTC);
-    RTC_Cmd(LPC_RTC, ENABLE);
-
-    RTC_TIME_Type RTCFullTime;
-    readTimeFromEEPROM(&RTCFullTime);
-//    RTCFullTime.HOUR = 4;
-//    RTCFullTime.MIN = 20;
-//    RTCFullTime.SEC = 0;
-    RTC_SetFullTime(LPC_RTC, &RTCFullTime);
-    RTC_GetFullTime(LPC_RTC, &RTCFullTime);
-
-    saveTimeToEEPROM(&RTCFullTime);
-}
-
-/* Funkcja do obsługi joysticka */
-static void handle_joystick(void) {
-    //uint8_t joystick_state = GPIO_ReadValue(1);
-	uint8_t read_position =  joystick_read();
-	if (!read_position){
-		return;
-	}
-	joystick_state = read_position;
-	if(joystick_state == JOYSTICK_CENTER){
-		edit_mode ^= 1;
-	}
-	if (edit_mode) {
-		if (joystick_state & JOYSTICK_UP) { // Joystick w górę
-			if (!menu_state) {
-                alarm_godziny = (uint8_t)((alarm_godziny + (uint8_t)(1)) % (uint8_t)(24));
-            }
-			else if (menu_state == (uint8_t)(1)) {
-                alarm_minuty = (uint8_t)((alarm_minuty + (uint8_t)(1)) % (uint8_t)(60));
-            }
-			else if (menu_state == (uint8_t)(2)) {
-                alarm_sekundy = (uint8_t)((alarm_sekundy + (uint8_t)(1)) % (uint8_t)(60));
-            }
-		} else if (joystick_state & JOYSTICK_DOWN) { // Joystick w dół
-			if (menu_state == (uint8_t)(0)) {
-                alarm_godziny = (alarm_godziny == (uint8_t)(0)) ? (uint8_t)(23) : (uint8_t) (alarm_godziny - (uint8_t)(1));
-            }
-			else if (menu_state == (uint8_t)(1)) {
-                alarm_minuty = (alarm_minuty == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (alarm_minuty - (uint8_t)(1));
-            }
-			else if (menu_state == (uint8_t)(2)) {
-                alarm_sekundy = (alarm_sekundy == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (alarm_sekundy - (uint8_t)(1));
-            }
-		} else if (joystick_state & JOYSTICK_RIGHT) { // Joystick w prawo
-			menu_state = (uint8_t)((menu_state + (uint8_t)(1)) % (uint8_t)(3));
-//			if (menu_state < 3) menu_state = (menu_state + 1) % 3;
-//			else {
-//				// W menu ustawiania budzika, joystick w prawo zatwierdza ustawienia i wraca do normalnego trybu wyświetlania czasu
-//				menu_state = 0;
-//			}
-		} else if (joystick_state & JOYSTICK_LEFT) { // Joystick w prawo
-			menu_state = (uint8_t)((menu_state - (uint8_t)(1)) % (uint8_t)(3));
-//			if (menu_state > 0) menu_state = (menu_state - 1) % 3;
-//			else {
-//				// W menu ustawiania budzika, joystick w prawo zatwierdza ustawienia i wraca do normalnego trybu wyświetlania czasu
-//				menu_state = 3;
-//			}
-		}
-    }
-}
-
-void saveTimeToEEPROM(const RTC_TIME_Type *rtc) {
-	uint8_t buf[3];
-	buf[0] = rtc->SEC;
-	buf[1] = rtc->MIN;
-	buf[2] = rtc->HOUR;
-	eeprom_write(buf, 0, 3);
-}
-
-void readTimeFromEEPROM(RTC_TIME_Type *rtc) {
-	uint8_t buf[3];
-	eeprom_read(buf, 0, 3);
-	rtc->SEC = buf[0];
-	rtc->MIN = buf[1];
-	rtc->HOUR = buf[2];
-}
-
-void saveAlarmToEEPROM(uint8_t hours,  uint8_t minutes, uint8_t seconds) {
-	uint8_t buf[3];
-	buf[0] = hours;
-	buf[1] = minutes;
-	buf[2] = seconds;
-	eeprom_write(buf, 3, 3);
-}
-
-//void readAlarmFromEEPROM(uint8_t *hours, uint8_t *minutes, uint8_t *seconds) {
-//
-//}
-
 uint32_t jasno;
 
 oled_color_t background = OLED_COLOR_BLACK;
 oled_color_t foreground = OLED_COLOR_WHITE;
 uint8_t prev_light = 0;
 uint8_t light_change = 0;
-
-static void setTime() {
-	uint8_t set_time = 0;
-	menu_state = 0;
-	uint8_t joystick_state;
-	uint8_t buff[20];
-	uint8_t g = RTC_GetTime(LPC_RTC, RTC_TIMETYPE_HOUR);
-	uint8_t m = RTC_GetTime(LPC_RTC, RTC_TIMETYPE_MINUTE);
-	uint8_t s = RTC_GetTime(LPC_RTC, RTC_TIMETYPE_SECOND);
-	oled_clearScreen(background);
-	while (!set_time) {
-		joystick_state = joystick_read();
-		if (joystick_state & JOYSTICK_UP) { // Joystick w górę
-			if (!menu_state) {
-				g = (uint8_t)((g + (uint8_t)(1)) % (uint8_t)(24));
-			}
-			else if (menu_state == (uint8_t)(1)) {
-				m = (uint8_t)((m + (uint8_t)(1)) % (uint8_t)(60));
-			}
-			else if (menu_state == (uint8_t)(2)) {
-				s = (uint8_t)((s + (uint8_t)(1)) % (uint8_t)(60));
-			}
-		} else if (joystick_state & JOYSTICK_DOWN) { // Joystick w dół
-			if (menu_state == (uint8_t)(0)) {
-				g = (g == (uint8_t)(0)) ? (uint8_t)(23) : (uint8_t) (g - (uint8_t)(1));
-			}
-			else if (menu_state == (uint8_t)(1)) {
-				m = (m == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (m - (uint8_t)(1));
-			}
-			else if (menu_state == (uint8_t)(2)) {
-				s = (s == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (s - (uint8_t)(1));
-			}
-		} else if (joystick_state & JOYSTICK_RIGHT) { // Joystick w prawo
-			menu_state = (uint8_t)((menu_state + (uint8_t)(1)) % (uint8_t)(3));
-		} else if (joystick_state & JOYSTICK_LEFT) { // Joystick w prawo
-			menu_state = (uint8_t)((menu_state - (uint8_t)(1)) % (uint8_t)(3));
-		}
-		snprintf(buff, sizeof(buff), "Ustaw: %02d:%02d:%02d", g, m, s);
-		oled_putString(1, 20, (uint8_t *) buff, foreground, background);
-		set_time = joystick_read() & JOYSTICK_CENTER;
-		Timer0_Wait(1);
-	}
-	RTC_TIME_Type RTCFullTime;
-	RTCFullTime.HOUR = g;
-	RTCFullTime.MIN = m;
-	RTCFullTime.SEC = s;
-	saveTimeToEEPROM(&RTCFullTime);
-	RTC_SetFullTime(LPC_RTC, &RTCFullTime);
-}
-
-
 
 int main (void) {
 
@@ -551,8 +240,325 @@ int main (void) {
 
 }
 
-void check_failed(uint8_t *file, uint32_t line)
-{
+static void setTime() {
+	uint8_t set_time = 0;
+	menu_state = 0;
+	uint8_t joystick_state;
+	uint8_t buff[20];
+	uint8_t g = RTC_GetTime(LPC_RTC, RTC_TIMETYPE_HOUR);
+	uint8_t m = RTC_GetTime(LPC_RTC, RTC_TIMETYPE_MINUTE);
+	uint8_t s = RTC_GetTime(LPC_RTC, RTC_TIMETYPE_SECOND);
+	oled_clearScreen(background);
+	while (!set_time) {
+		joystick_state = joystick_read();
+		if (joystick_state & JOYSTICK_UP) { // Joystick w górę
+			if (!menu_state) {
+				g = (uint8_t)((g + (uint8_t)(1)) % (uint8_t)(24));
+			}
+			else if (menu_state == (uint8_t)(1)) {
+				m = (uint8_t)((m + (uint8_t)(1)) % (uint8_t)(60));
+			}
+			else if (menu_state == (uint8_t)(2)) {
+				s = (uint8_t)((s + (uint8_t)(1)) % (uint8_t)(60));
+			}
+		} else if (joystick_state & JOYSTICK_DOWN) { // Joystick w dół
+			if (menu_state == (uint8_t)(0)) {
+				g = (g == (uint8_t)(0)) ? (uint8_t)(23) : (uint8_t) (g - (uint8_t)(1));
+			}
+			else if (menu_state == (uint8_t)(1)) {
+				m = (m == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (m - (uint8_t)(1));
+			}
+			else if (menu_state == (uint8_t)(2)) {
+				s = (s == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (s - (uint8_t)(1));
+			}
+		} else if (joystick_state & JOYSTICK_RIGHT) { // Joystick w prawo
+			menu_state = (uint8_t)((menu_state + (uint8_t)(1)) % (uint8_t)(3));
+		} else if (joystick_state & JOYSTICK_LEFT) { // Joystick w prawo
+			menu_state = (uint8_t)((menu_state - (uint8_t)(1)) % (uint8_t)(3));
+		}
+		snprintf(buff, sizeof(buff), "Ustaw: %02d:%02d:%02d", g, m, s);
+		oled_putString(1, 20, (uint8_t *) buff, foreground, background);
+		set_time = joystick_read() & JOYSTICK_CENTER;
+		Timer0_Wait(1);
+	}
+	RTC_TIME_Type RTCFullTime;
+	RTCFullTime.HOUR = g;
+	RTCFullTime.MIN = m;
+	RTCFullTime.SEC = s;
+	saveTimeToEEPROM(&RTCFullTime);
+	RTC_SetFullTime(LPC_RTC, &RTCFullTime);
+}
+
+/* Funkcja do obsługi joysticka */
+static void handle_joystick(void) {
+    //uint8_t joystick_state = GPIO_ReadValue(1);
+	uint8_t read_position =  joystick_read();
+	if (!read_position){
+		return;
+	}
+	joystick_state = read_position;
+	if(joystick_state == JOYSTICK_CENTER){
+		edit_mode ^= 1;
+	}
+	if (edit_mode) {
+		if (joystick_state & JOYSTICK_UP) { // Joystick w górę
+			if (!menu_state) {
+                alarm_godziny = (uint8_t)((alarm_godziny + (uint8_t)(1)) % (uint8_t)(24));
+            }
+			else if (menu_state == (uint8_t)(1)) {
+                alarm_minuty = (uint8_t)((alarm_minuty + (uint8_t)(1)) % (uint8_t)(60));
+            }
+			else if (menu_state == (uint8_t)(2)) {
+                alarm_sekundy = (uint8_t)((alarm_sekundy + (uint8_t)(1)) % (uint8_t)(60));
+            }
+		} else if (joystick_state & JOYSTICK_DOWN) { // Joystick w dół
+			if (menu_state == (uint8_t)(0)) {
+                alarm_godziny = (alarm_godziny == (uint8_t)(0)) ? (uint8_t)(23) : (uint8_t) (alarm_godziny - (uint8_t)(1));
+            }
+			else if (menu_state == (uint8_t)(1)) {
+                alarm_minuty = (alarm_minuty == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (alarm_minuty - (uint8_t)(1));
+            }
+			else if (menu_state == (uint8_t)(2)) {
+                alarm_sekundy = (alarm_sekundy == (uint8_t)(0)) ? (uint8_t)(59) : (uint8_t) (alarm_sekundy - (uint8_t)(1));
+            }
+		} else if (joystick_state & JOYSTICK_RIGHT) { // Joystick w prawo
+			menu_state = (uint8_t)((menu_state + (uint8_t)(1)) % (uint8_t)(3));
+//			if (menu_state < 3) menu_state = (menu_state + 1) % 3;
+//			else {
+//				// W menu ustawiania budzika, joystick w prawo zatwierdza ustawienia i wraca do normalnego trybu wyświetlania czasu
+//				menu_state = 0;
+//			}
+		} else if (joystick_state & JOYSTICK_LEFT) { // Joystick w prawo
+			menu_state = (uint8_t)((menu_state - (uint8_t)(1)) % (uint8_t)(3));
+//			if (menu_state > 0) menu_state = (menu_state - 1) % 3;
+//			else {
+//				// W menu ustawiania budzika, joystick w prawo zatwierdza ustawienia i wraca do normalnego trybu wyświetlania czasu
+//				menu_state = 3;
+//			}
+		}
+    }
+}
+
+/* Inicjalizacja RTC */
+static void setUpRTC(void) {
+
+    RTC_Init(LPC_RTC);
+    RTC_ResetClockTickCounter(LPC_RTC);
+    RTC_Cmd(LPC_RTC, ENABLE);
+
+    RTC_TIME_Type RTCFullTime;
+    readTimeFromEEPROM(&RTCFullTime);
+//    RTCFullTime.HOUR = 4;
+//    RTCFullTime.MIN = 20;
+//    RTCFullTime.SEC = 0;
+    RTC_SetFullTime(LPC_RTC, &RTCFullTime);
+    RTC_GetFullTime(LPC_RTC, &RTCFullTime);
+
+    saveTimeToEEPROM(&RTCFullTime);
+}
+
+static void saveTimeToEEPROM(const RTC_TIME_Type *rtc) {
+	uint8_t buf[3];
+	buf[0] = rtc->SEC;
+	buf[1] = rtc->MIN;
+	buf[2] = rtc->HOUR;
+	eeprom_write(buf, 0, 3);
+}
+
+static void readTimeFromEEPROM(RTC_TIME_Type *rtc) {
+	uint8_t buf[3];
+	eeprom_read(buf, 0, 3);
+	rtc->SEC = buf[0];
+	rtc->MIN = buf[1];
+	rtc->HOUR = buf[2];
+}
+
+static void saveAlarmToEEPROM(uint8_t hours,  uint8_t minutes, uint8_t seconds) {
+	uint8_t buf[3];
+	buf[0] = hours;
+	buf[1] = minutes;
+	buf[2] = seconds;
+	eeprom_write(buf, 3, 3);
+}
+
+//void readAlarmFromEEPROM(uint8_t *hours, uint8_t *minutes, uint8_t *seconds) {
+//
+//}
+
+static void init_ssp(void) {
+	SSP_CFG_Type SSP_ConfigStruct;
+	PINSEL_CFG_Type PinCfg;
+
+	/*
+	 * Initialize SPI pin connect
+	 * P0.7 - SCK;
+	 * P0.8 - MISO
+	 * P0.9 - MOSI
+	 * P2.2 - SSEL - used as GPIO
+	 */
+	PinCfg.Funcnum = 2;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 7;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 8;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 9;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Funcnum = 0;
+	PinCfg.Portnum = 2;
+	PinCfg.Pinnum = 2;
+	PINSEL_ConfigPin(&PinCfg);
+
+	SSP_ConfigStructInit(&SSP_ConfigStruct);
+
+	// Initialize SSP peripheral with parameter given in structure above
+	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
+
+	// Enable SSP peripheral
+	SSP_Cmd(LPC_SSP1, ENABLE);
+
+}
+
+static void init_i2c(void) {
+	PINSEL_CFG_Type PinCfg;
+
+	/* Initialize I2C2 pin connect */
+	PinCfg.Funcnum = 2;
+	PinCfg.Pinnum = 10;
+	PinCfg.Portnum = 0;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 11;
+	PINSEL_ConfigPin(&PinCfg);
+
+	// Initialize I2C peripheral
+	I2C_Init(LPC_I2C2, 100000);
+
+	/* Enable I2C1 operation */
+	I2C_Cmd(LPC_I2C2, ENABLE);
+}
+
+static void init_adc(void) {
+	PINSEL_CFG_Type PinCfg;
+
+	/*
+	 * Init ADC pin connect
+	 * AD0.0 on P0.23
+	 */
+	PinCfg.Funcnum = 1;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Pinnum = 23;
+	PinCfg.Portnum = 0;
+	PINSEL_ConfigPin(&PinCfg);
+
+	/* Configuration for ADC :
+	 * 	Frequency at 0.2Mhz
+	 *  ADC channel 0, no Interrupt
+	 */
+	ADC_Init(LPC_ADC, 200000);
+	ADC_IntConfig(LPC_ADC,ADC_CHANNEL_0,DISABLE);
+	ADC_ChannelCmd(LPC_ADC,ADC_CHANNEL_0,ENABLE);
+
+}
+
+static void playNote(uint32_t note, uint32_t durationMs) {
+
+    uint32_t t = 0;
+
+    if (note > 0) {
+
+        while (t < (durationMs*1000)) {
+            NOTE_PIN_HIGH();
+            Timer0_us_Wait(note / 2);
+            //delay32Us(0, note / 2);
+
+            NOTE_PIN_LOW();
+            Timer0_us_Wait(note / 2);
+            //delay32Us(0, note / 2);
+
+            t += note;
+        }
+
+    }
+    else {
+    	Timer0_Wait(durationMs);
+        //delay32Ms(0, durationMs);
+    }
+}
+
+static uint32_t getNote(uint8_t ch) {
+    if (ch >= 'A' && ch <= 'G')
+        return notes[ch - 'A'];
+
+    if (ch >= 'a' && ch <= 'g')
+        return notes[ch - 'a' + 7];
+
+    return 0;
+}
+
+static uint32_t getDuration(uint8_t ch) {
+    if (ch < '0' || ch > '9')
+        return 400;
+
+    /* number of ms */
+
+    return (ch - '0') * 200;
+}
+
+static uint32_t getPause(uint8_t ch) {
+    switch (ch) {
+    case '+':
+        return 0;
+    case ',':
+        return 5;
+    case '.':
+        return 20;
+    case '_':
+        return 30;
+    default:
+        return 5;
+    }
+}
+
+static void playSong(uint8_t *song) {
+    uint32_t note = 0;
+    uint32_t dur  = 0;
+    uint32_t pause = 0;
+
+    /*
+     * A song is a collection of tones where each tone is
+     * a note, duration and pause, e.g.
+     *
+     * "E2,F4,"
+     */
+
+    while(*song != '\0') {
+        note = getNote(*song++);
+        if (*song == '\0')
+            break;
+        dur  = getDuration(*song++);
+        if (*song == '\0')
+            break;
+        pause = getPause(*song++);
+
+        playNote(note, dur);
+        //delay32Ms(0, pause);
+        Timer0_Wait(pause);
+
+    }
+}
+
+void SysTick_Handler(void) {
+    msTicks++;
+}
+
+uint32_t getTicks(void) {
+    return msTicks;
+}
+
+void check_failed(uint8_t *file, uint32_t line) {
 	/* User can add his own implementation to report the file name and line number,
 	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
